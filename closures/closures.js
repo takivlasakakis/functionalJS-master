5726b05b36988d1cbbed0f63644f215445b69236
Taki Vlasakakis <takivlasakakis@Takis-MacBook-Pro.local>
2017-09-12 16:32:15 -0600

    stuff

--- closures/closures.js
+++ closures/closures.js
@@ -100,3 +100,42 @@ var Toaster = function() {
 
 var myToaster = Toaster();
 myToaster.setTemp(300);
+
+
+// ###### ###### ###### ###### ###### ###### ######
+
+// #Callback Exercises
+
+// Write a function, funcCaller, that takes a func (a function) and an arg (any data type). The function returns the func called with arg(as an argument).
+
+var funcCaller = function(func, arg) {
+	return func(arg);
+};
+
+// Write a function, firstVal, that takes an array, arr, and a function, func, and calls func with the first index of the arr, the index # and the whole array.
+
+var firstVal = function(arr, func) {
+	func(arr[0], 0, arr);
+};
+
+// Change firstVal to work not only with arrays but also objects. Since objects are not ordered, you can use any key-value pair on the object.
+
+firstVal = function(list, func) {
+	if(Array.isArray(list)) {
+		return func(arr[0], 0, arr);
+	} else {
+		// for(var k in list) {
+		// 	return func(list[k], k, list);
+		// }
+		var propArr = Object.keys(list);
+		func(list[propArr[0]], propArr[0], list);
+	}
+};
+
+// [Extra Credit] Write a function, once, (see: http://underscorejs.org/#once) that takes a function and returns a version of that function which can only be called once. [Hint: you need a closure] You probably don't want to be able to double charge someone's credit card. Here is an example of how to use it:
+
+//   var chargeCreditCard = function(num, price){
+//     //charges credit card for a certain price
+//   };
+//   var processPaymentOnce = once(chargeCreditCard);
+//   processPaymentOnce(123456789012, 200);
